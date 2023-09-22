a0d4b949c74f8b6b580c0463d8bed8ffa10c405e
yasin <yasinmiapolash@gmail.com>
2023-09-22 12:56:46 +0600

    navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,120 @@
+//Navigation ----one page---another page
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
-
       home: HomeScreen(),
-
     );
   }
-
-  
-
 }
 
-
+///Route
 class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
-  List<String> Students=[
-    'Yasin',
-    'polASH',
-    'Yasin',
-    'polASH',
-    'Yasin',
-    'polASH',
-    'Yasin',
-    'polASH',
-
-
-
-  ];
   @override
   Widget build(BuildContext context) {
-
-    // TODO: implement build
     return Scaffold(
-      appBar: AppBar(title: Text('Home'),),
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Home',
+              style: TextStyle(fontSize: 40),
+            ),
 
-      body: Scrollbar(
-        thickness: 20,
-        radius: Radius.circular(15),
-        child:ListView.separated(
-          itemCount: Students.length,
-          itemBuilder: (context,index){
-            return ListTile(
-              title: Text(Students[index]),
-            );
-          },
-          separatorBuilder: (context,index){
-            return Divider();
-          },
+            ///Navigation route A ->route B
+            ///Navigator
+            ElevatedButton(
+                onPressed: () {
+                  ///Navigation route HomeScreen  ->route Settings
+                  ///Navigator
+
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => SettingScreen()));
+                },
+                child: const Text('Go to setting screen')),
+            ElevatedButton(
+                onPressed: () {
+                  ///Navigation route HomeScreen  ->route Settings
+                  ///Navigator
+
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => OrderScreen()));
+                },
+                child: const Text('Go to Order screen')),
+          ],
         ),
       ),
     );
   }
+}
 
+///Route
+class SettingScreen extends StatelessWidget {
+  const SettingScreen({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Setting screen'),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Home',
+              style: TextStyle(fontSize: 40),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
+class OrderScreen extends StatelessWidget {
+  const OrderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Order Processing '),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Order Processing ',
+              style: TextStyle(fontSize: 40),
+            ),
+            TextButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => SettingScreen()));
+                },
+                child: const Text('go to settings'))
+          ],
+        ),
+      ),
+    );
+  }
 }
