ba6ea7de4d7c66249a48daaa5a5eaf98defb6d82
yasin <yasinmiapolash@gmail.com>
2023-09-22 14:57:26 +0600

    alert and snack bar

--- lib/main.dart
+++ lib/main.dart
@@ -19,21 +19,50 @@ class MyApp extends StatelessWidget {
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  MySnakbar(massage ,context){
+    return ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(content: Text(massage))
+    );
+
+  }
+
+  MyAlertDiolog(context) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return  Expanded(
+              child: AlertDialog(
+            title: Text('Alert!!'),
+            content: Text('Do you want to delete'),
+                actions: [
+                  TextButton(onPressed: (){
+
+                    MySnakbar('this is delete',context);
+                  }, child: Text('Yes')),
+                  TextButton(onPressed: (){
+                    Navigator.of(context).pop();
+
+                  }, child: Text('No'))
+                ],
+          ));
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home'),
+        title: Text('Alert'),
       ),
       body: Center(
-        child: const Column(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-
-
-            ]
-            ),
-
+            ElevatedButton(
+                onPressed: () {
+                  MyAlertDiolog(context);
+                },
+                child: Text('Click me'))
           ],
         ),
       ),
