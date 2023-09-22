adc80d0d0689a8bd99721fb5ab9f083337dbbac6
yasin <yasinmiapolash@gmail.com>
2023-09-22 13:02:22 +0600

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,3 @@
-//Navigation ----one page---another page
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -26,92 +23,17 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
+        title: const Text('Home'),
       ),
       body: Center(
-        child: Column(
+        child: const Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(
-              'Home',
-              style: TextStyle(fontSize: 40),
+
+
+            ]
             ),
 
-            ///Navigation route A ->route B
-            ///Navigator
-            ElevatedButton(
-                onPressed: () {
-                  ///Navigation route HomeScreen  ->route Settings
-                  ///Navigator
-
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => SettingScreen()));
-                },
-                child: const Text('Go to setting screen')),
-            ElevatedButton(
-                onPressed: () {
-                  ///Navigation route HomeScreen  ->route Settings
-                  ///Navigator
-
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => OrderScreen()));
-                },
-                child: const Text('Go to Order screen')),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-///Route
-class SettingScreen extends StatelessWidget {
-  const SettingScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Setting screen'),
-      ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Home',
-              style: TextStyle(fontSize: 40),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class OrderScreen extends StatelessWidget {
-  const OrderScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Order Processing '),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Order Processing ',
-              style: TextStyle(fontSize: 40),
-            ),
-            TextButton(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => SettingScreen()));
-                },
-                child: const Text('go to settings'))
           ],
         ),
       ),
