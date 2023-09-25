fe4e77fad481cee93a3ca17a2b439feafeb94018
yasin <yasinmiapolash@gmail.com>
2023-09-25 23:47:16 +0600

    listview

--- lib/main.dart
+++ lib/main.dart
@@ -17,54 +17,55 @@ class MyApp extends StatelessWidget {
 
 ///Route
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+   HomeScreen({super.key});
 
-  MySnakbar(massage ,context){
+  var MyItem=[
+    {"img":"https://plus.unsplash.com/premium_photo-1675034393381-7e246fc40755?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80","titel":"Yasin",},
+    {"img":"https://plus.unsplash.com/premium_photo-1675034393381-7e246fc40755?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80","titel":"Rakib",},
+    {"img":"https://plus.unsplash.com/premium_photo-1675034393381-7e246fc40755?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80","titel":"Rasel",},
+    {"img":"https://plus.unsplash.com/premium_photo-1675034393381-7e246fc40755?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80","titel":"Rab",},
+    {"img":"https://plus.unsplash.com/premium_photo-1675034393381-7e246fc40755?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80","titel":"Rashed",},
+    {"img":"https://plus.unsplash.com/premium_photo-1675034393381-7e246fc40755?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80","titel":"Imtiaz",},
+
+  ];
+
+  MySnakbar(context,sms){
+
     return ScaffoldMessenger.of(context).showSnackBar(
-    SnackBar(content: Text(massage))
+
+      SnackBar(
+          duration:Duration(seconds: 1) ,
+          content: Text(sms))
     );
-
-  }
-
-  MyAlertDiolog(context) {
-    return showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return  Expanded(
-              child: AlertDialog(
-            title: Text('Alert!!'),
-            content: Text('Do you want to delete'),
-                actions: [
-                  TextButton(onPressed: (){
-
-                    MySnakbar('this is delete',context);
-                  }, child: Text('Yes')),
-                  TextButton(onPressed: (){
-                    Navigator.of(context).pop();
-
-                  }, child: Text('No'))
-                ],
-          ));
-        });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Alert'),
+        title: Text('My App'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-                onPressed: () {
-                  MyAlertDiolog(context);
-                },
-                child: Text('Click me'))
-          ],
-        ),
+      body:ListView.builder(
+          itemCount: MyItem.length,
+          itemBuilder:(context,index){
+            return GestureDetector(
+              onTap: (){
+
+                MySnakbar(context, MyItem[index]['titel']);
+
+
+              },
+              child: Container(
+
+                margin: const EdgeInsets.all(10),
+                width: double.infinity,
+                height: 400,
+                child: Image.network(MyItem[index]['img']!,),
+
+              ),
+            );
+          }
+
       ),
     );
   }
